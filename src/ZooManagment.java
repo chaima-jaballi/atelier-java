162e6f1c7c414fc5dc9e6ff32c03df598328c8cc
chaima-jaballi <chaima.jaballi@esprit.tn>
2024-09-16 19:33:33 +0100

    exercice zoo

---
+++ src/ZooManagment.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ZooManagment {
+    public static void main(String[] args) {
+        int nbrCages=20;
+        String zooName="my zoo";
+
+        public static void main(String[] args){
+            scanner sc = new scanner(system)
+            ZooManagment zm = new ZooManagment();
+            System.out.printf(zm.ZooName+"comporte"+zm.nbrCages);
+        }
+
+
+    }
+}
